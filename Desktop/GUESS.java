643bbd8e1f3b3eb6280b91afccec7a8375eff34c
slavsenkute <bsenkute@stkentigernsacademy.org.uk>
2016-06-23 16:37:42 +0100

    Version1

---
+++ Desktop/GUESS.java
@@ -0,0 +1,36 @@
+public class GUESS
+{
+    public void openingmessage()
+    {
+        //System.out.println
+    }
+    
+    public void playgame()
+    {
+       //3.1   	Display Menu
+       //3.2   	If option is equal to play then
+       //3.3               	Enter player name
+       //3.4               	Generate random number
+       //3.4.1	Computer generates a number between 1 and 50       	
+       //3.5               	While found equals false do
+       //3.6   	        	        	enter lucky guess from player
+       //3.7               	        	if guess equals random number then
+       //3.8                           	        	set found to true
+       //3.9               	        	else if guess is less than random number then
+       //3.10             	        	        	display a less than message
+       //3.11 	        	        	else
+       //3.12             	        	        	display a more than message
+       //3.13             	        	end if
+       //3.14 	        	end while
+       //3.15             	Display results
+       //3.16 	else
+       //3.17             	quit game
+       //3.18 	end if
+
+    }
+    
+    public void closingmessage()
+    {
+        //System.out.println	
+    }
+}
